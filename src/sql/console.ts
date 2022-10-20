60a2b33b6eb4a9a236c9ead1a95e967aaa524824
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2022-10-20 22:27:44 +0800

    feat：新增sql

--- src/sql/console.ts
+++ src/sql/console.ts
@@ -1,12 +1,21 @@
 export const consoleSql = {
-  queryOverviewData:"select count(hid) as warehouses from warehouse;"+
-    "select count(1) as staffs from `user` where usrType != 'admin';"+
+  queryOverviewData:
+    "select count(hid) as warehouses from warehouse;" +
+    "select count(1) as staffs from `user` where usrType != 'admin';" +
     "select count(isPop) as total,isPop from trading where `time` like ? group by isPop",
-  
-  queryLatestTrading:"select date_format(`time`,'%m-%d') as `date`,`count`,isPop from trading "+
-    "where date_format(`time`,'%Y-%m-%d') between ? and ? "+
-    "group by `date`,`count`,isPop desc",
-  
-  queryCapacityUsage:"select sum(`count`) as total,wh.houseName from store as st "+
-    "left join warehouse as wh on st.hid = wh.hid group by st.hid"
-}
+
+  queryLatestTrading:
+    "select date_format(`time`,'%m-%d') as `date`,`count`,isPop from trading " +
+    "where date_format(`time`,'%Y-%m-%d') between ? and ? " +
+    "order by `date` desc",
+
+  queryCapacityUsage:
+    "select sum(`count`) as total,wh.houseName from store as st " +
+    "left join warehouse as wh on st.hid = wh.hid group by st.hid",
+  queryAllWarehouse: "select hid,houseName,capacity from warehouse",
+  queryLatestTradingLog:
+    "select wh.houseName,cg.cname,tr.count,tr.isPop,tr.time,usr.usrName " +
+    "from trading as tr left join warehouse as wh on tr.hid = wh.hid " +
+    "left join `user` as usr on tr.uid = usr.uid left join cargo as cg on tr.cid = cg.cid " +
+    "order by tr.time desc limit 10",
+};
--- src/sql/pmc.ts
+++ src/sql/pmc.ts
@@ -1,16 +1,25 @@
 export const pmcSql = {
-  addCargoType:"insert into `cargotypes`(`typeName`) values(?)",
+  addCargoType: "insert into `cargotypes`(`typeName`) values(?)",
 
-  queryUsageCargoTypes:"select cts.ctid,cts.typeName,count(1) as `num` from cargo_type as ct "+
+  queryUsageCargoTypes:
+    "select cts.ctid,cts.typeName,count(1) as `num` from cargo_type as ct " +
     "left join cargotypes as cts on ct.ctid = cts.ctid group by ct.ctid",
-  
-  queryAllCargoTypes:"select ctid,typeName from cargotypes",
 
-  queryCargos:`select ct.cid,cg.cname,ct.ctid,cts.typeName from cargo_type as ct `+
+  queryAllCargoTypes: "select ctid,typeName from cargotypes",
+
+  queryCargos:
+    `select ct.cid,cg.cname,ct.ctid,cts.typeName from cargo_type as ct ` +
     `left join cargotypes as cts on ct.ctid = cts.ctid left join cargo as cg on ct.cid = cg.cid`,
-  queryCid:"select distinct cid,quantity from cargo",
+  queryCid: "select distinct cid,quantity from cargo",
 
-  addCargo:"insert into `cargo`(`cid`,`cname`) values(?,?)",
+  addCargo: "insert into `cargo`(`cid`,`cname`) values(?,?)",
   addCargo_Type: "insert into `cargo_type`(`cid`,`ctid`) values(?,?)",
-  delCargo_Type: "delete from `cargo_type` where cid=? and ctid=?"
+  delCargo_Type: "delete from `cargo_type` where cid=? and ctid=?",
+
+  queryDistributeData:
+    "select sto.count,wh.houseName from store as sto " +
+    "left join warehouse as wh on sto.hid = wh.hid where cid = ?",
+
+  delCargo:
+    "delete from trading where cid = :cid;delete from cargo_type where cid = :cid;delete from cargo where cid = :cid",
 };
